28a459034fe1e24d2d5486f94eb7044d83f77152
virgílio conceição césar <virgilio_cesar.dev@outlook.com>
2024-08-16 14:02:06 -0300

    separação do modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Menu from "./Menu";
 import Footer from "./components/Footer";
+import Modal from "./components/Modal";
 
 export default function App() {
-  return <div className="text-3 font-bold underline">
-    <Header />
-    <Menu />
-    <Footer />
-  </div>;
+  const[modal, setModal] = useState("hidden");
+  return (
+    <div className="text-3 font-bold underline">
+      <Header />
+      <Menu />
+      <Modal modal ={modal} setModal={setModal}/>
+      <Footer setModal={setModal} />
+    </div>
+  );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 
-const Footer = () => {
-  const [modal, setModal] = useState("hidden");
+const Footer = ({ setModal }: { setModal: (value : string) => void }) => {
   return (
     <>
       <footer className="w-full bg-red-500 py-3 fixed bottom-0 z-40 flex items-center justify-center">
@@ -15,41 +13,6 @@ const Footer = () => {
         </button>
       </footer>
       {/*---------------MODAL----------------- */}
-      <div
-        id="cart-modal"
-        className={`bg-black/50 w-full h-full fixed top-0 left-0 z-[99] items-center justify-center ${modal}`}
-      >
-        <div className="bg-white p-5 rounded-md min-w-[90%] md:min-w-[600px]">
-          <h2 className="text-center font-bold text-2xl mb-2">Meu carrinho</h2>
-          <div
-            id="cart-items"
-            className="flex justify-between mb-2 flex-col"
-          ></div>
-          <p className="font-bold">
-            Total: <span id="cart-total">0.00</span>
-          </p>
-          <p className="font-bold mt-4">Endereço de entrega:</p>
-          <input
-            type="text"
-            placeholder="Digite seu endereço completo..."
-            id="adrress"
-            className="w-full border-2 p-1 rounded my-1"
-          />
-          <p className="text-red-500 hidden" id="address-warn">
-            Digite seu endereço completo!
-          </p>
-          <div className="flex items-center justify-between mt-5 w-full">
-            <button id="close-modal-btn">Fechar</button>
-            <button
-              id="checkout-btn"
-              className="bg-green-500 text-white px-4 py1 rounded"
-            >
-              Finalizar pedido
-            </button>
-          </div>
-        </div>
-      </div>
-      
     </>
   );
 };
